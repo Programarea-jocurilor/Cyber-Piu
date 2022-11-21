a653ffa260be46458e74edc41fbea55944037309
MunteanuBianca <ioana.2013.bm@gmail.com>
2022-11-21 19:47:16 +0200

    Add function to fighting animation key event

--- Assets/Scripts/PatrollingEnemy/MeleeEnemy.cs
+++ Assets/Scripts/PatrollingEnemy/MeleeEnemy.cs
@@ -14,32 +14,34 @@ public class MeleeEnemy : MonoBehaviour
 
     //References
     private Animator anim;
+    private int playerHealth;
+    private EnemyPatrol enemyPatrol;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
-
+        
         //Attack only when the player is in sight
         if (PlayerInSight())
         {
+            //Attack after attackCooldown seconds
             if(cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 anim.SetTrigger("fighting");
             }
-        }    
+        } 
+
+        // if (enemyPatrol != null)  
+        // enemyPatrol.enabled = !PlayerInSight();
     }
 
     private bool PlayerInSight()
@@ -48,6 +50,8 @@ public class MeleeEnemy : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * (-transform.localScale.x) * colliderDistance, 
                 new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
                 0, Vector2.left, 0, playerLayer);
+        if(hit.collider != null)
+            playerHealth = PlayerPrefs.GetInt("currentHealth");
 
         return hit.collider != null;
     }
@@ -61,6 +65,9 @@ public class MeleeEnemy : MonoBehaviour
 
     private void DamagePlayer()
     {
-        //damage player
+        if(PlayerInSight())
+        {
+            playerHealth = playerHealth - damage;
+        }
     }
 }
