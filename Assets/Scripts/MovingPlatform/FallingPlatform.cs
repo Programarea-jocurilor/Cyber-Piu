ab744be96a914889af231f1336a9824ce015b286
MunteanuBianca <ioana.2013.bm@gmail.com>
2022-12-23 14:29:29 +0200

    Extend Map and FallingPlatforms Respawn

--- Assets/Scripts/MovingPlatform/FallingPlatform.cs
+++ Assets/Scripts/MovingPlatform/FallingPlatform.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     Rigidbody2D rb;
+    Vector2 initialPosition;
+    bool platformMovingBack;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (platformMovingBack)
+            transform.position = Vector2.MoveTowards(transform.position, initialPosition, 20f * Time.deltaTime);
+
+        if (transform.position.y == initialPosition.y)
+            platformMovingBack = false;
     }
 
     void OnCollisionEnter2D( Collision2D collider)
@@ -16,12 +28,21 @@ public class FallingPlatform : MonoBehaviour
         if (collider.gameObject.name == "PlayerCaracter")
         {
             Invoke ("DropPlatform", 0.5f);
-            Destroy (gameObject, 2f);
+            // Destroy (gameObject, 2f);
         }
     }
 
     void DropPlatform()
     {
         rb.isKinematic = false;
+        Invoke("GetPlatformBack", 5f);
     }
+
+    void GetPlatformBack()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        platformMovingBack = true;
+    }
+
 }
