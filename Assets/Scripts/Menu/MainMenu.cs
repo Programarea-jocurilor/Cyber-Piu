15254cb099b0618fc9e30c5c343fe1d0e765e56a
Grejdan Mirolena <mirolenavioleta.mg@gmail.com>
2023-01-13 01:37:23 +0200

    Pause menu and some debugging

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -10,7 +10,55 @@ public class MainMenu : MonoBehaviour
      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
 
    }
+   
+  //  ***
+       public static bool gameIsPaused;
 
+    [SerializeField] private GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameIsPaused=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() //ca sa o pot folosi si pentru Resume Button
+    {   
+        pauseMenu.SetActive(false);
+        Time.timeScale=1f;
+        gameIsPaused=false;
+    }
+
+    public void Pause()
+    {    Debug.Log("Pauza");
+     pauseMenu.SetActive(true);
+     Time.timeScale=0f;
+     gameIsPaused=true;
+    }
+ public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale=1;
+        Debug.Log("Merge");
+    }
+
+// ****
    public void QuitGame()
    {
     Debug.Log("QUIT");
