bbdc70461417ab697897e257972d5d1014ee97ec
MunteanuBianca <ioana.2013.bm@gmail.com>
2023-01-13 16:41:51 +0200

    Add disolve shader effect on player death

---
+++ Assets/Disolve.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disolve : MonoBehaviour
+{
+    Material material;
+
+    bool isDissiolving = false;
+    float fade =1f;
+
+    public Health playerHealth;
+
+    private void Awake()
+    {
+        playerHealth = GetComponent<Health>();
+    }
+
+    void Start()
+    {
+        // Get reference to the material
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update()
+    {
+        if (playerHealth.currentHealth == 0)
+        {
+            isDissiolving = true;
+            StartCoroutine(WaitAndDie());
+        }   
+
+        if (isDissiolving)
+        {
+            fade -= Time.deltaTime;
+
+            if(fade <= 0f)
+            {
+                fade = 0f;
+                isDissiolving = false;
+            }
+
+            //Set the property
+            material.SetFloat("_Fade", fade);
+        }
+    }
+
+    IEnumerator WaitAndDie()
+    {
+        yield return new WaitForSeconds(1);
+        this.gameObject.SetActive(false);
+    }
+}
